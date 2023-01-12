ff37a5114b91968550a89fd9a0f36888ec4f67a4
Anshuman202 <asanshuman2@gmail.com>
2023-01-12 23:30:55 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welocome to Stack & Queue Programs Using Linked list *****");
+
+        }
+    }
+}
+
